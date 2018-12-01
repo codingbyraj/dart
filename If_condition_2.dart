4fbeab6b5ec96673a4c9a1eb5bcae30b1846c906
Verma-A <aman.verma@live.in>
2018-12-01 13:42:08 +0530

    all type of if conditions

--- If_condition_2.dart
+++ If_condition_2.dart
@@ -1,8 +1,26 @@
 void main() {
   int a = 20;
+
+// This is single if condition
+  if (a > 10) {
+    print('$a is greater than 10');
+  }
+
+// This is if with else
   if (a >= 18) {
     print('you are eligible for voting');
-  }else{
+  } else {
     print('Your are not eligible for voting');
+  }
+
+  // This is else if lader
+  if (a < 5) {
+    print('$a is less than 5');
+  } else if (a < 10) {
+    print('$a is less than 10');
+  } else if (a < 20) {
+    print('$a is less than 20');
+  } else if (a < 25) {
+    print('$a is less than 25');
   }
 }
